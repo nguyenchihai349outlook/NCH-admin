de08c776155c03acfb54857d180e019f3d28e257
nguyenchihai349outlook <166897865+nguyenchihai349outlook@users.noreply.github.com>
2024-05-02 22:09:38 +0700

    Initialize backport/pr-3476-to-release/8.0

---
+++ src/Aspire.Dashboard/Components/Controls/LogViewer.razor.cs
@@ -0,0 +1,158 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Globalization;
+using Aspire.Dashboard.ConsoleLogs;
+using Aspire.Dashboard.Extensions;
+using Aspire.Dashboard.Model;
+using Aspire.Dashboard.Utils;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Aspire.Dashboard.Components;
+
+/// <summary>
+/// A log viewing UI component that shows a live view of a log, with syntax highlighting and automatic scrolling.
+/// </summary>
+public sealed partial class LogViewer
+{
+    private readonly CancellationSeries _cancellationSeries = new();
+    private bool _convertTimestampsFromUtc;
+    private bool _applicationChanged;
+
+    [Inject]
+    public required BrowserTimeProvider TimeProvider { get; init; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (_applicationChanged)
+        {
+            await JS.InvokeVoidAsync("resetContinuousScrollPosition");
+            _applicationChanged = false;
+        }
+        if (firstRender)
+        {
+            await JS.InvokeVoidAsync("initializeContinuousScroll");
+        }
+    }
+
+    private readonly List<LogEntry> _logEntries = new();
+    private int? _baseLineNumber;
+
+    internal async Task SetLogSourceAsync(IAsyncEnumerable<IReadOnlyList<ResourceLogLine>> batches, bool convertTimestampsFromUtc)
+    {
+        _convertTimestampsFromUtc = convertTimestampsFromUtc;
+
+        var cancellationToken = await _cancellationSeries.NextAsync();
+        var logParser = new LogParser();
+
+        await foreach (var batch in batches.WithCancellation(cancellationToken))
+        {
+            if (batch.Count is 0)
+            {
+                continue;
+            }
+
+            foreach (var (lineNumber, content, isErrorOutput) in batch)
+            {
+                // Keep track of the base line number to ensure that we can calculate the line number of each log entry.
+                // This becomes important when the total number of log entries exceeds the limit and is truncated.
+                if (_baseLineNumber is null)
+                {
+                    _baseLineNumber = lineNumber;
+                }
+
+                InsertSorted(_logEntries, logParser.CreateLogEntry(content, isErrorOutput));
+            }
+
+            StateHasChanged();
+        }
+    }
+
+    private void InsertSorted(List<LogEntry> logEntries, LogEntry logEntry)
+    {
+        if (logEntry.ParentId != null)
+        {
+            // If we have a parent id, then we know we're on a non-timestamped line that is part
+            // of a multi-line log entry. We need to find the prior line from that entry
+            for (var rowIndex = logEntries.Count - 1; rowIndex >= 0; rowIndex--)
+            {
+                var current = logEntries[rowIndex];
+
+                if (current.Id == logEntry.ParentId && logEntry.LineIndex - 1 == current.LineIndex)
+                {
+                    InsertLogEntry(logEntries, rowIndex + 1, logEntry);
+                    return;
+                }
+            }
+        }
+        else if (logEntry.Timestamp != null)
+        {
+            // Otherwise, if we have a timestamped line, we just need to find the prior line.
+            // Since the rows are always in order, as soon as we see a timestamp
+            // that is less than the one we're adding, we can insert it immediately after that
+            for (var rowIndex = logEntries.Count - 1; rowIndex >= 0; rowIndex--)
+            {
+                var current = logEntries[rowIndex];
+                var currentTimestamp = current.Timestamp ?? current.ParentTimestamp;
+
+                if (currentTimestamp != null && currentTimestamp < logEntry.Timestamp)
+                {
+                    InsertLogEntry(logEntries, rowIndex + 1, logEntry);
+                    return;
+                }
+            }
+        }
+
+        // If we didn't find a place to insert then append it to the end. This happens with the first entry, but
+        // could also happen if the logs don't have recognized timestamps.
+        InsertLogEntry(logEntries, logEntries.Count, logEntry);
+
+        void InsertLogEntry(List<LogEntry> logEntries, int index, LogEntry logEntry)
+        {
+            // Set the line number of the log entry.
+            if (index == 0)
+            {
+                Debug.Assert(_baseLineNumber != null, "Should be set before this method is run.");
+                logEntry.LineNumber = _baseLineNumber.Value;
+            }
+            else
+            {
+                logEntry.LineNumber = logEntries[index - 1].LineNumber + 1;
+            }
+
+            logEntries.Insert(index, logEntry);
+
+            // If a log entry isn't inserted at the end then update the line numbers of all subsequent entries.
+            for (var i = index + 1; i < logEntries.Count; i++)
+            {
+                logEntries[i].LineNumber++;
+            }
+        }
+    }
+
+    private string GetDisplayTimestamp(DateTimeOffset timestamp)
+    {
+        if (_convertTimestampsFromUtc)
+        {
+            timestamp = TimeProvider.ToLocal(timestamp);
+        }
+
+        return timestamp.ToString(KnownFormats.ConsoleLogsTimestampFormat, CultureInfo.InvariantCulture);
+    }
+
+    internal async Task ClearLogsAsync()
+    {
+        await _cancellationSeries.ClearAsync();
+
+        _applicationChanged = true;
+        _logEntries.Clear();
+        StateHasChanged();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _cancellationSeries.ClearAsync();
+    }
+}
---
+++ src/Aspire.Hosting/DistributedApplicationBuilder.cs
@@ -0,0 +1,303 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Dashboard;
+using Aspire.Hosting.Dcp;
+using Aspire.Hosting.Lifecycle;
+using Aspire.Hosting.Publishing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Aspire.Hosting;
+
+/// <summary>
+/// A builder for creating instances of <see cref="DistributedApplication"/>.
+/// </summary>
+public class DistributedApplicationBuilder : IDistributedApplicationBuilder
+{
+    private const string HostingDiagnosticListenerName = "Aspire.Hosting";
+    private const string ApplicationBuildingEventName = "DistributedApplicationBuilding";
+    private const string ApplicationBuiltEventName = "DistributedApplicationBuilt";
+    private const string BuilderConstructingEventName = "DistributedApplicationBuilderConstructing";
+    private const string BuilderConstructedEventName = "DistributedApplicationBuilderConstructed";
+
+    private readonly HostApplicationBuilder _innerBuilder;
+
+    /// <inheritdoc />
+    public IHostEnvironment Environment => _innerBuilder.Environment;
+
+    /// <inheritdoc />
+    public ConfigurationManager Configuration => _innerBuilder.Configuration;
+
+    /// <inheritdoc />
+    public IServiceCollection Services => _innerBuilder.Services;
+
+    /// <inheritdoc />
+    public string AppHostDirectory { get; }
+
+    /// <inheritdoc />
+    public DistributedApplicationExecutionContext ExecutionContext { get; }
+
+    /// <inheritdoc />
+    public IResourceCollection Resources { get; } = new ResourceCollection();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DistributedApplicationBuilder"/> class with the specified options.
+    /// </summary>
+    /// <param name="args">The arguments provided to the builder.</param>
+    public DistributedApplicationBuilder(string[] args) : this(new DistributedApplicationOptions { Args = args })
+    {
+        ArgumentNullException.ThrowIfNull(args);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DistributedApplicationBuilder"/> class with the specified options.
+    /// </summary>
+    /// <param name="options">The options for the distributed application.</param>
+    public DistributedApplicationBuilder(DistributedApplicationOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        var innerBuilderOptions = new HostApplicationBuilderSettings();
+        LogBuilderConstructing(options, innerBuilderOptions);
+        _innerBuilder = new HostApplicationBuilder(innerBuilderOptions);
+
+        _innerBuilder.Logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Warning);
+        _innerBuilder.Logging.AddFilter("Microsoft.AspNetCore.Server.Kestrel", LogLevel.Error);
+
+        // This is so that we can see certificate errors in the resource server in the console logs.
+        // See: https://github.com/dotnet/aspire/issues/2914
+        _innerBuilder.Logging.AddFilter("Microsoft.AspNetCore.Server.Kestrel.Core.KestrelServer", LogLevel.Warning);
+
+        AppHostDirectory = options.ProjectDirectory ?? _innerBuilder.Environment.ContentRootPath;
+
+        // Set configuration
+        ConfigurePublishingOptions(options);
+        _innerBuilder.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
+        {
+            // Make the app host directory available to the application via configuration
+            ["AppHost:Directory"] = AppHostDirectory
+        });
+
+        ExecutionContext = _innerBuilder.Configuration["Publishing:Publisher"] switch
+        {
+            "manifest" => new DistributedApplicationExecutionContext(DistributedApplicationOperation.Publish),
+            _ => new DistributedApplicationExecutionContext(DistributedApplicationOperation.Run)
+        };
+
+        // Core things
+        _innerBuilder.Services.AddSingleton(sp => new DistributedApplicationModel(Resources));
+        _innerBuilder.Services.AddHostedService<DistributedApplicationLifecycle>();
+        _innerBuilder.Services.AddHostedService<DistributedApplicationRunner>();
+        _innerBuilder.Services.AddSingleton(options);
+        _innerBuilder.Services.AddSingleton<ResourceNotificationService>();
+        _innerBuilder.Services.AddSingleton<ResourceLoggerService>();
+
+        if (ExecutionContext.IsRunMode)
+        {
+            // Dashboard
+            if (!options.DisableDashboard)
+            {
+                if (!IsDashboardUnsecured(_innerBuilder.Configuration))
+                {
+                    // Set a random API key for the OTLP exporter.
+                    // Passed to apps as a standard OTEL attribute to include in OTLP requests and the dashboard to validate.
+                    _innerBuilder.Configuration.AddInMemoryCollection(
+                        new Dictionary<string, string?>
+                        {
+                            ["AppHost:OtlpApiKey"] = TokenGenerator.GenerateToken()
+                        }
+                    );
+
+                    if (_innerBuilder.Configuration[KnownConfigNames.DashboardFrontendBrowserToken] is not { Length: > 0 } browserToken)
+                    {
+                        browserToken = TokenGenerator.GenerateToken();
+                    }
+
+                    // Set a random API key for the OTLP exporter.
+                    // Passed to apps as a standard OTEL attribute to include in OTLP requests and the dashboard to validate.
+                    _innerBuilder.Configuration.AddInMemoryCollection(
+                        new Dictionary<string, string?>
+                        {
+                            ["AppHost:BrowserToken"] = browserToken
+                        }
+                    );
+                }
+
+                _innerBuilder.Services.AddOptions<TransportOptions>().ValidateOnStart().PostConfigure(MapTransportOptionsFromCustomKeys);
+                _innerBuilder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IValidateOptions<TransportOptions>, TransportOptionsValidator>());
+                _innerBuilder.Services.AddSingleton<DashboardServiceHost>();
+                _innerBuilder.Services.AddHostedService(sp => sp.GetRequiredService<DashboardServiceHost>());
+                _innerBuilder.Services.AddSingleton<IDashboardEndpointProvider, HostDashboardEndpointProvider>();
+                _innerBuilder.Services.AddLifecycleHook<DashboardLifecycleHook>();
+                _innerBuilder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<DashboardOptions>, ConfigureDefaultDashboardOptions>());
+                _innerBuilder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IValidateOptions<DashboardOptions>, ValidateDashboardOptions>());
+            }
+
+            // DCP stuff
+            _innerBuilder.Services.AddSingleton<ApplicationExecutor>();
+            _innerBuilder.Services.AddSingleton<IDcpDependencyCheckService, DcpDependencyCheck>();
+            _innerBuilder.Services.AddHostedService<DcpHostService>();
+            _innerBuilder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IConfigureOptions<DcpOptions>, ConfigureDefaultDcpOptions>());
+            _innerBuilder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IValidateOptions<DcpOptions>, ValidateDcpOptions>());
+
+            // We need a unique path per application instance
+            _innerBuilder.Services.AddSingleton(new Locations());
+            _innerBuilder.Services.AddSingleton<IKubernetesService, KubernetesService>();
+        }
+
+        // Publishing support
+        _innerBuilder.Services.AddLifecycleHook<Http2TransportMutationHook>();
+        _innerBuilder.Services.AddKeyedSingleton<IDistributedApplicationPublisher, ManifestPublisher>("manifest");
+
+        _innerBuilder.Services.AddSingleton(ExecutionContext);
+        LogBuilderConstructed(this);
+    }
+
+    private void MapTransportOptionsFromCustomKeys(TransportOptions options)
+    {
+        if (Configuration.GetBool(KnownConfigNames.AllowUnsecuredTransport) is { } allowUnsecuredTransport)
+        {
+            options.AllowUnsecureTransport = allowUnsecuredTransport;
+        }
+    }
+
+    private static bool IsDashboardUnsecured(IConfiguration configuration)
+    {
+        return configuration.GetBool(KnownConfigNames.DashboardUnsecuredAllowAnonymous) ?? false;
+    }
+
+    private void ConfigurePublishingOptions(DistributedApplicationOptions options)
+    {
+        var switchMappings = new Dictionary<string, string>()
+        {
+            { "--publisher", "Publishing:Publisher" },
+            { "--output-path", "Publishing:OutputPath" },
+            { "--dcp-cli-path", "DcpPublisher:CliPath" },
+            { "--container-runtime", "DcpPublisher:ContainerRuntime" },
+            { "--dependency-check-timeout", "DcpPublisher:DependencyCheckTimeout" },
+        };
+        _innerBuilder.Configuration.AddCommandLine(options.Args ?? [], switchMappings);
+        _innerBuilder.Services.Configure<PublishingOptions>(_innerBuilder.Configuration.GetSection(PublishingOptions.Publishing));
+    }
+
+    /// <inheritdoc />
+    public DistributedApplication Build()
+    {
+        AspireEventSource.Instance.DistributedApplicationBuildStart();
+        try
+        {
+            LogAppBuilding(this);
+
+            // AddResource(resource) validates that a name is unique but it's possible to add resources directly to the resource collection.
+            // Validate names for duplicates while building the application.
+            foreach (var duplicateResourceName in Resources.GroupBy(r => r.Name, StringComparers.ResourceName)
+                .Where(g => g.Count() > 1)
+                .Select(g => g.Key))
+            {
+                throw new DistributedApplicationException($"Multiple resources with the name '{duplicateResourceName}'. Resource names are case-insensitive.");
+            }
+
+            var application = new DistributedApplication(_innerBuilder.Build());
+            LogAppBuilt(application);
+            return application;
+        }
+        finally
+        {
+            AspireEventSource.Instance.DistributedApplicationBuildStop();
+        }
+    }
+
+    /// <inheritdoc />
+    public IResourceBuilder<T> AddResource<T>(T resource) where T : IResource
+    {
+        ArgumentNullException.ThrowIfNull(resource);
+
+        if (Resources.FirstOrDefault(r => string.Equals(r.Name, resource.Name, StringComparisons.ResourceName)) is { } existingResource)
+        {
+            throw new DistributedApplicationException($"Cannot add resource of type '{resource.GetType()}' with name '{resource.Name}' because resource of type '{existingResource.GetType()}' with that name already exists. Resource names are case-insensitive.");
+        }
+
+        Resources.Add(resource);
+        return CreateResourceBuilder(resource);
+    }
+
+    /// <inheritdoc />
+    public IResourceBuilder<T> CreateResourceBuilder<T>(T resource) where T : IResource
+    {
+        ArgumentNullException.ThrowIfNull(resource);
+
+        var builder = new DistributedApplicationResourceBuilder<T>(this, resource);
+        return builder;
+    }
+
+    private static DiagnosticListener LogBuilderConstructing(DistributedApplicationOptions appBuilderOptions, HostApplicationBuilderSettings hostBuilderOptions)
+    {
+        var diagnosticListener = new DiagnosticListener(HostingDiagnosticListenerName);
+
+        if (diagnosticListener.IsEnabled() && diagnosticListener.IsEnabled(BuilderConstructingEventName))
+        {
+            Write(diagnosticListener, BuilderConstructingEventName, (appBuilderOptions, hostBuilderOptions));
+        }
+
+        return diagnosticListener;
+    }
+
+    private static DiagnosticListener LogBuilderConstructed(DistributedApplicationBuilder builder)
+    {
+        var diagnosticListener = new DiagnosticListener(HostingDiagnosticListenerName);
+
+        if (diagnosticListener.IsEnabled() && diagnosticListener.IsEnabled(BuilderConstructedEventName))
+        {
+            Write(diagnosticListener, BuilderConstructedEventName, builder);
+        }
+
+        return diagnosticListener;
+    }
+
+    private static DiagnosticListener LogAppBuilding(DistributedApplicationBuilder appBuilder)
+    {
+        var diagnosticListener = new DiagnosticListener(HostingDiagnosticListenerName);
+
+        if (diagnosticListener.IsEnabled() && diagnosticListener.IsEnabled(ApplicationBuildingEventName))
+        {
+            Write(diagnosticListener, ApplicationBuildingEventName, appBuilder);
+        }
+
+        return diagnosticListener;
+    }
+
+    private static DiagnosticListener LogAppBuilt(DistributedApplication app)
+    {
+        var diagnosticListener = new DiagnosticListener(HostingDiagnosticListenerName);
+
+        if (diagnosticListener.IsEnabled() && diagnosticListener.IsEnabled(ApplicationBuiltEventName))
+        {
+            Write(diagnosticListener, ApplicationBuiltEventName, app);
+        }
+
+        return diagnosticListener;
+    }
+
+    // Remove when https://github.com/dotnet/runtime/pull/78532 is merged and consumed by the used SDK.
+#if NET7_0
+        [UnconditionalSuppressMessage("AOT", "IL3050:RequiresDynamicCode",
+            Justification = "DiagnosticSource is used here to pass objects in-memory to code using HostFactoryResolver. This won't require creating new generic types.")]
+#endif
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026:UnrecognizedReflectionPattern",
+        Justification = "The values being passed into Write are being consumed by the application already.")]
+    private static void Write<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)] T>(
+        DiagnosticListener diagnosticSource,
+        string name,
+        T value)
+    {
+        diagnosticSource.Write(name, value);
+    }
+}
