e23783fd0510854c4a8bf587764cfb8170c86c63
nguyenchihai349outlook <166897865+nguyenchihai349outlook@users.noreply.github.com>
2024-05-02 22:10:18 +0700

    Initialize vNext

---
+++ tests/Aspire.Dashboard.Tests/TelemetryRepositoryTests/MetricsTests.cs
@@ -0,0 +1,384 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.InteropServices;
+using Aspire.Dashboard.Otlp.Model;
+using Aspire.Dashboard.Otlp.Model.MetricValues;
+using Aspire.Dashboard.Otlp.Storage;
+using Google.Protobuf.Collections;
+using OpenTelemetry.Proto.Metrics.V1;
+using Xunit;
+using static Aspire.Dashboard.Tests.TelemetryRepositoryTests.TestHelpers;
+
+namespace Aspire.Dashboard.Tests.TelemetryRepositoryTests;
+
+public class MetricsTests
+{
+    private static readonly DateTime s_testTime = new(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+    [Fact]
+    public void AddMetrics()
+    {
+        // Arrange
+        var repository = CreateRepository();
+
+        // Act
+        var addContext = new AddContext();
+        repository.AddMetrics(addContext, new RepeatedField<ResourceMetrics>()
+        {
+            new ResourceMetrics
+            {
+                Resource = CreateResource(),
+                ScopeMetrics =
+                {
+                    new ScopeMetrics
+                    {
+                        Scope = CreateScope(name: "test-meter"),
+                        Metrics =
+                        {
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(1)),
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(2)),
+                            CreateSumMetric(metricName: "test2", startTime: s_testTime.AddMinutes(1)),
+                        }
+                    },
+                    new ScopeMetrics
+                    {
+                        Scope = CreateScope(name: "test-meter2"),
+                        Metrics =
+                        {
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(1))
+                        }
+                    }
+                }
+            }
+        });
+
+        // Assert
+        Assert.Equal(0, addContext.FailureCount);
+
+        var applications = repository.GetApplications();
+        Assert.Collection(applications,
+            app =>
+            {
+                Assert.Equal("TestService", app.ApplicationName);
+                Assert.Equal("TestId", app.InstanceId);
+            });
+
+        var instruments = repository.GetInstrumentsSummary(applications[0].InstanceId);
+        Assert.Collection(instruments,
+            instrument =>
+            {
+                Assert.Equal("test", instrument.Name);
+                Assert.Equal("Test metric description", instrument.Description);
+                Assert.Equal("widget", instrument.Unit);
+                Assert.Equal("test-meter", instrument.Parent.MeterName);
+                Assert.Empty(instrument.Dimensions);
+                Assert.Empty(instrument.KnownAttributeValues);
+            },
+            instrument =>
+            {
+                Assert.Equal("test2", instrument.Name);
+                Assert.Equal("Test metric description", instrument.Description);
+                Assert.Equal("widget", instrument.Unit);
+                Assert.Equal("test-meter", instrument.Parent.MeterName);
+                Assert.Empty(instrument.Dimensions);
+                Assert.Empty(instrument.KnownAttributeValues);
+            },
+            instrument =>
+            {
+                Assert.Equal("test", instrument.Name);
+                Assert.Equal("Test metric description", instrument.Description);
+                Assert.Equal("widget", instrument.Unit);
+                Assert.Equal("test-meter2", instrument.Parent.MeterName);
+                Assert.Empty(instrument.Dimensions);
+                Assert.Empty(instrument.KnownAttributeValues);
+            });
+    }
+
+    [Fact]
+    public void AddMetrics_AttributeLimits_LimitsApplied()
+    {
+        // Arrange
+        var repository = CreateRepository(maxAttributeCount: 5, maxAttributeLength: 16);
+
+        var metricAttributes = new List<KeyValuePair<string, string>>();
+        var meterAttributes = new List<KeyValuePair<string, string>>();
+
+        for (var i = 0; i < 10; i++)
+        {
+            var value = GetValue((i + 1) * 5);
+            metricAttributes.Add(new KeyValuePair<string, string>($"Metric_Key{i}", value));
+            meterAttributes.Add(new KeyValuePair<string, string>($"Meter_Key{i}", value));
+        }
+
+        // Act
+        var addContext = new AddContext();
+        repository.AddMetrics(addContext, new RepeatedField<ResourceMetrics>()
+        {
+            new ResourceMetrics
+            {
+                Resource = CreateResource(),
+                ScopeMetrics =
+                {
+                    new ScopeMetrics
+                    {
+                        Scope = CreateScope(name: "test-meter", attributes: meterAttributes),
+                        Metrics =
+                        {
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(1), attributes: metricAttributes)
+                        }
+                    }
+                }
+            }
+        });
+
+        // Assert
+        Assert.Equal(0, addContext.FailureCount);
+
+        var applications = repository.GetApplications();
+        Assert.Collection(applications,
+            app =>
+            {
+                Assert.Equal("TestService", app.ApplicationName);
+                Assert.Equal("TestId", app.InstanceId);
+            });
+
+        var instrument = repository.GetInstrument(new GetInstrumentRequest
+        {
+            ApplicationServiceId = applications[0].InstanceId,
+            InstrumentName = "test",
+            MeterName = "test-meter",
+            StartTime = DateTime.MinValue,
+            EndTime = DateTime.MaxValue
+        })!;
+
+        Assert.Collection(MemoryMarshal.ToEnumerable(instrument.Parent.Attributes),
+            p =>
+            {
+                Assert.Equal("Meter_Key0", p.Key);
+                Assert.Equal("01234", p.Value);
+            },
+            p =>
+            {
+                Assert.Equal("Meter_Key1", p.Key);
+                Assert.Equal("0123456789", p.Value);
+            },
+            p =>
+            {
+                Assert.Equal("Meter_Key2", p.Key);
+                Assert.Equal("012345678901234", p.Value);
+            },
+            p =>
+            {
+                Assert.Equal("Meter_Key3", p.Key);
+                Assert.Equal("0123456789012345", p.Value);
+            },
+            p =>
+            {
+                Assert.Equal("Meter_Key4", p.Key);
+                Assert.Equal("0123456789012345", p.Value);
+            });
+
+        var dimensionAttributes = instrument.Dimensions.Single().Key;
+
+        Assert.Collection(MemoryMarshal.ToEnumerable(dimensionAttributes),
+            p =>
+            {
+                Assert.Equal("Metric_Key0", p.Key);
+                Assert.Equal("01234", p.Value);
+            },
+            p =>
+            {
+                Assert.Equal("Metric_Key1", p.Key);
+                Assert.Equal("0123456789", p.Value);
+            },
+            p =>
+            {
+                Assert.Equal("Metric_Key2", p.Key);
+                Assert.Equal("012345678901234", p.Value);
+            },
+            p =>
+            {
+                Assert.Equal("Metric_Key3", p.Key);
+                Assert.Equal("0123456789012345", p.Value);
+            },
+            p =>
+            {
+                Assert.Equal("Metric_Key4", p.Key);
+                Assert.Equal("0123456789012345", p.Value);
+            });
+    }
+
+    [Fact]
+    public void RoundtripSeconds()
+    {
+        var start = s_testTime.AddMinutes(1);
+        var nanoSeconds = DateTimeToUnixNanoseconds(start);
+        var end = OtlpHelpers.UnixNanoSecondsToDateTime(nanoSeconds);
+        Assert.Equal(start, end);
+    }
+
+    [Fact]
+    public void GetInstrument()
+    {
+        // Arrange
+        var repository = CreateRepository();
+
+        // Act
+        var addContext = new AddContext();
+        repository.AddMetrics(addContext, new RepeatedField<ResourceMetrics>()
+        {
+            new ResourceMetrics
+            {
+                Resource = CreateResource(),
+                ScopeMetrics =
+                {
+                    new ScopeMetrics
+                    {
+                        Scope = CreateScope(name: "test-meter"),
+                        Metrics =
+                        {
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(1)),
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(2)),
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(1), attributes: [KeyValuePair.Create("key1", "value1")]),
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(1), attributes: [KeyValuePair.Create("key1", "value2")]),
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(1), attributes: [KeyValuePair.Create("key1", "value1"), KeyValuePair.Create("key2", "value1")])
+                        }
+                    }
+                }
+            }
+        });
+
+        // Assert
+        Assert.Equal(0, addContext.FailureCount);
+
+        var applications = repository.GetApplications();
+        Assert.Collection(applications,
+            app =>
+            {
+                Assert.Equal("TestService", app.ApplicationName);
+                Assert.Equal("TestId", app.InstanceId);
+            });
+
+        var instrument = repository.GetInstrument(new GetInstrumentRequest
+        {
+            ApplicationServiceId = applications[0].InstanceId,
+            InstrumentName = "test",
+            MeterName = "test-meter",
+            StartTime = s_testTime.AddMinutes(1),
+            EndTime = s_testTime.AddMinutes(1.5),
+        });
+
+        Assert.NotNull(instrument);
+        Assert.Equal("test", instrument.Name);
+        Assert.Equal("Test metric description", instrument.Description);
+        Assert.Equal("widget", instrument.Unit);
+        Assert.Equal("test-meter", instrument.Parent.MeterName);
+
+        Assert.Collection(instrument.KnownAttributeValues.OrderBy(kvp => kvp.Key),
+            e =>
+            {
+                Assert.Equal("key1", e.Key);
+                Assert.Equal(new [] { "", "value1", "value2" }, e.Value);
+            },
+            e =>
+            {
+                Assert.Equal("key2", e.Key);
+                Assert.Equal(new[] { "", "value1" }, e.Value);
+            });
+
+        Assert.Equal(4, instrument.Dimensions.Count);
+
+        AssertDimensionValues(instrument.Dimensions, Array.Empty<KeyValuePair<string, string>>(), valueCount: 1);
+        AssertDimensionValues(instrument.Dimensions, new KeyValuePair<string, string>[] { KeyValuePair.Create("key1", "value1") }, valueCount: 1);
+        AssertDimensionValues(instrument.Dimensions, new KeyValuePair<string, string>[] { KeyValuePair.Create("key1", "value2") }, valueCount: 1);
+        AssertDimensionValues(instrument.Dimensions, new KeyValuePair<string, string>[] { KeyValuePair.Create("key1", "value1"), KeyValuePair.Create("key2", "value1") }, valueCount: 1);
+    }
+
+    [Fact]
+    public void AddMetrics_Capacity_ValuesRemoved()
+    {
+        // Arrange
+        var repository = CreateRepository(maxMetricsCount: 3);
+
+        // Act
+        var addContext = new AddContext();
+        repository.AddMetrics(addContext, new RepeatedField<ResourceMetrics>()
+        {
+            new ResourceMetrics
+            {
+                Resource = CreateResource(),
+                ScopeMetrics =
+                {
+                    new ScopeMetrics
+                    {
+                        Scope = CreateScope(name: "test-meter"),
+                        Metrics =
+                        {
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(1), value: 1),
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(2), value: 2),
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(3), value: 3),
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(4), value: 4),
+                            CreateSumMetric(metricName: "test", startTime: s_testTime.AddMinutes(5), value: 5),
+                        }
+                    }
+                }
+            }
+        });
+
+        // Assert
+        Assert.Equal(0, addContext.FailureCount);
+
+        var applications = repository.GetApplications();
+        Assert.Collection(applications,
+            app =>
+            {
+                Assert.Equal("TestService", app.ApplicationName);
+                Assert.Equal("TestId", app.InstanceId);
+            });
+
+        var instrument = repository.GetInstrument(new GetInstrumentRequest
+        {
+            ApplicationServiceId = applications[0].InstanceId,
+            InstrumentName = "test",
+            MeterName = "test-meter",
+            StartTime = DateTime.MinValue,
+            EndTime = DateTime.MaxValue
+        })!;
+
+        Assert.Equal("test", instrument.Name);
+        Assert.Equal("Test metric description", instrument.Description);
+        Assert.Equal("widget", instrument.Unit);
+        Assert.Equal("test-meter", instrument.Parent.MeterName);
+
+        // Only the last 3 values should be kept.
+        var dimension = Assert.Single(instrument.Dimensions);
+        Assert.Collection(dimension.Value.Values,
+            m =>
+            {
+                Assert.Equal(s_testTime.AddMinutes(2), m.Start);
+                Assert.Equal(s_testTime.AddMinutes(3), m.End);
+                Assert.Equal(3, ((MetricValue<long>)m).Value);
+            },
+            m =>
+            {
+                Assert.Equal(s_testTime.AddMinutes(3), m.Start);
+                Assert.Equal(s_testTime.AddMinutes(4), m.End);
+                Assert.Equal(4, ((MetricValue<long>)m).Value);
+            },
+            m =>
+            {
+                Assert.Equal(s_testTime.AddMinutes(4), m.Start);
+                Assert.Equal(s_testTime.AddMinutes(5), m.End);
+                Assert.Equal(5, ((MetricValue<long>)m).Value);
+            });
+    }
+
+    private static void AssertDimensionValues(Dictionary<ReadOnlyMemory<KeyValuePair<string, string>>, DimensionScope> dimensions, ReadOnlyMemory<KeyValuePair<string, string>> key, int valueCount)
+    {
+        var scope = dimensions[key];
+        Assert.True(Enumerable.SequenceEqual(MemoryMarshal.ToEnumerable(key), scope.Attributes), "Key and attributes don't match.");
+
+        Assert.Equal(valueCount, scope.Values.Count);
+    }
+}
---
+++ tests/Aspire.EndToEnd.Tests/IntegrationServicesTests.cs
@@ -0,0 +1,113 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.InteropServices;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+using Aspire.TestProject;
+using Aspire.Workload.Tests;
+
+namespace Aspire.EndToEnd.Tests;
+
+public class IntegrationServicesTests : IClassFixture<IntegrationServicesFixture>
+{
+    private readonly IntegrationServicesFixture _integrationServicesFixture;
+    private readonly TestOutputWrapper _testOutput;
+
+    public IntegrationServicesTests(ITestOutputHelper testOutput, IntegrationServicesFixture integrationServicesFixture)
+    {
+        _integrationServicesFixture = integrationServicesFixture;
+        _testOutput = new TestOutputWrapper(testOutput);
+    }
+
+    [Theory]
+    [Trait("scenario", "basicservices")]
+    [InlineData(TestResourceNames.mongodb)]
+    [InlineData(TestResourceNames.mysql)]
+    [InlineData(TestResourceNames.efmysql)]
+    [InlineData(TestResourceNames.postgres)]
+    [InlineData(TestResourceNames.efnpgsql)]
+    [InlineData(TestResourceNames.rabbitmq)]
+    [InlineData(TestResourceNames.redis)]
+    [InlineData(TestResourceNames.sqlserver)]
+    public Task VerifyComponentWorks(TestResourceNames resourceName)
+        => RunTestAsync(async () =>
+        {
+            _integrationServicesFixture.EnsureAppHasResources(resourceName);
+            try
+            {
+                var response = await _integrationServicesFixture.IntegrationServiceA.HttpGetAsync("http", $"/{resourceName}/verify");
+                var responseContent = await response.Content.ReadAsStringAsync();
+
+                Assert.True(response.IsSuccessStatusCode, responseContent);
+            }
+            catch
+            {
+                await _integrationServicesFixture.DumpComponentLogsAsync(resourceName, _testOutput);
+                throw;
+            }
+        });
+
+    [ConditionalFact]
+    [SkipOnCI("https://github.com/dotnet/aspire/issues/3161")]
+    [Trait("scenario", "oracle")]
+    public Task VerifyOracleComponentWorks()
+        => VerifyComponentWorks(TestResourceNames.oracledatabase);
+
+    [ConditionalFact]
+    [Trait("scenario", "cosmos")]
+    public Task VerifyCosmosComponentWorks()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX) && RuntimeInformation.ProcessArchitecture == Architecture.Arm64)
+        {
+            throw SkipException.ForSkip($"Skipping 'cosmos' test because the emulator isn't supported on macOS ARM64.");
+        }
+
+        return VerifyComponentWorks(TestResourceNames.cosmos);
+    }
+
+    [Fact]
+    [Trait("scenario", "basicservices")]
+    public Task KafkaComponentCanProduceAndConsume()
+        => RunTestAsync(async() =>
+        {
+            _integrationServicesFixture.EnsureAppHasResources(TestResourceNames.kafka);
+            string topic = $"topic-{Guid.NewGuid()}";
+
+            var response = await _integrationServicesFixture.IntegrationServiceA.HttpGetAsync("http", $"/kafka/produce/{topic}");
+            var responseContent = await response.Content.ReadAsStringAsync();
+            Assert.True(response.IsSuccessStatusCode, responseContent);
+
+            response = await _integrationServicesFixture.IntegrationServiceA.HttpGetAsync("http", $"/kafka/consume/{topic}");
+            responseContent = await response.Content.ReadAsStringAsync();
+            Assert.True(response.IsSuccessStatusCode, responseContent);
+        });
+
+    [Fact]
+    // Include all the scenarios here so this test gets run for all of them.
+    [Trait("scenario", "cosmos")]
+    [Trait("scenario", "oracle")]
+    [Trait("scenario", "basicservices")]
+    public Task VerifyHealthyOnIntegrationServiceA()
+        => RunTestAsync(async () =>
+        {
+            // We wait until timeout for the /health endpoint to return successfully. We assume
+            // that components wired up into this project have health checks enabled.
+            await _integrationServicesFixture.IntegrationServiceA.WaitForHealthyStatusAsync("http", _testOutput);
+        });
+
+    private async Task RunTestAsync(Func<Task> test)
+    {
+        _integrationServicesFixture.Project.EnsureAppHostRunning();
+        try
+        {
+            await test();
+        }
+        catch
+        {
+            await _integrationServicesFixture.Project.DumpDockerInfoAsync();
+            throw;
+        }
+    }
+}
