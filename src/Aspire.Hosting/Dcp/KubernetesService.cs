6291e8b65af88123f1f2decc47dd17a6167840af
nguyenchihai349outlook <166897865+nguyenchihai349outlook@users.noreply.github.com>
2024-05-02 22:09:14 +0700

    Initialize jamesnk/otlpapikey-morebits

---
+++ src/Aspire.Hosting/Dcp/KubernetesService.cs
@@ -0,0 +1,365 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Immutable;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using Aspire.Hosting.Dcp.Model;
+using k8s;
+using k8s.Exceptions;
+using k8s.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Polly;
+using Polly.Retry;
+
+namespace Aspire.Hosting.Dcp;
+
+internal enum DcpApiOperationType
+{
+    Create = 1,
+    List = 2,
+    Delete = 3,
+    Watch = 4,
+    GetLogSubresource = 5,
+}
+
+internal interface IKubernetesService
+{
+    Task<T> CreateAsync<T>(T obj, CancellationToken cancellationToken = default)
+        where T : CustomResource;
+    Task<List<T>> ListAsync<T>(string? namespaceParameter = null, CancellationToken cancellationToken = default)
+        where T : CustomResource;
+    Task<T> DeleteAsync<T>(string name, string? namespaceParameter = null, CancellationToken cancellationToken = default)
+        where T : CustomResource;
+    IAsyncEnumerable<(WatchEventType, T)> WatchAsync<T>(
+        string? namespaceParameter = null,
+        CancellationToken cancellationToken = default)
+        where T : CustomResource;
+    Task<Stream> GetLogStreamAsync<T>(
+        T obj,
+        string logStreamType,
+        bool? follow = true,
+        bool? timestamps = false,
+        CancellationToken cancellationToken = default) where T : CustomResource;
+}
+
+internal sealed class KubernetesService(ILogger<KubernetesService> logger, IOptions<DcpOptions> dcpOptions, Locations locations) : IKubernetesService, IDisposable
+{
+    private static readonly TimeSpan s_initialRetryDelay = TimeSpan.FromMilliseconds(100);
+    private static GroupVersion GroupVersion => Model.Dcp.GroupVersion;
+
+    private DcpKubernetesClient? _kubernetes;
+
+    public TimeSpan MaxRetryDuration { get; set; } = TimeSpan.FromSeconds(20);
+
+    public Task<T> CreateAsync<T>(T obj, CancellationToken cancellationToken = default)
+        where T : CustomResource
+    {
+        var resourceType = GetResourceFor<T>();
+        var namespaceParameter = obj.Namespace();
+
+        return ExecuteWithRetry(
+           DcpApiOperationType.Create,
+           resourceType,
+           async (kubernetes) =>
+           {
+               var response = string.IsNullOrEmpty(namespaceParameter)
+                ? await kubernetes.CustomObjects.CreateClusterCustomObjectWithHttpMessagesAsync(
+                    obj,
+                    GroupVersion.Group,
+                    GroupVersion.Version,
+                    resourceType,
+                    cancellationToken: cancellationToken).ConfigureAwait(false)
+                : await kubernetes.CustomObjects.CreateNamespacedCustomObjectWithHttpMessagesAsync(
+                    obj,
+                    GroupVersion.Group,
+                    GroupVersion.Version,
+                    namespaceParameter,
+                    resourceType,
+                    cancellationToken: cancellationToken).ConfigureAwait(false);
+
+               return KubernetesJson.Deserialize<T>(response.Body.ToString());
+           },
+           cancellationToken);
+    }
+
+    public Task<List<T>> ListAsync<T>(string? namespaceParameter = null, CancellationToken cancellationToken = default)
+        where T : CustomResource
+    {
+        var resourceType = GetResourceFor<T>();
+
+        return ExecuteWithRetry(
+            DcpApiOperationType.List,
+            resourceType,
+            async (kubernetes) =>
+            {
+                var response = string.IsNullOrEmpty(namespaceParameter)
+                    ? await kubernetes.CustomObjects.ListClusterCustomObjectWithHttpMessagesAsync(
+                        GroupVersion.Group,
+                        GroupVersion.Version,
+                        resourceType,
+                        cancellationToken: cancellationToken).ConfigureAwait(false)
+                    : await kubernetes.CustomObjects.ListNamespacedCustomObjectWithHttpMessagesAsync(
+                        GroupVersion.Group,
+                        GroupVersion.Version,
+                        namespaceParameter,
+                        resourceType,
+                        cancellationToken: cancellationToken).ConfigureAwait(false);
+
+                return KubernetesJson.Deserialize<CustomResourceList<T>>(response.Body.ToString()).Items;
+            },
+            cancellationToken);
+    }
+
+    public Task<T> DeleteAsync<T>(string name, string? namespaceParameter = null, CancellationToken cancellationToken = default)
+        where T : CustomResource
+    {
+        var resourceType = GetResourceFor<T>();
+
+        return ExecuteWithRetry(
+            DcpApiOperationType.Delete,
+            resourceType,
+            async (kubernetes) =>
+            {
+                var response = string.IsNullOrEmpty(namespaceParameter)
+                ? await kubernetes.CustomObjects.DeleteClusterCustomObjectWithHttpMessagesAsync(
+                    GroupVersion.Group,
+                    GroupVersion.Version,
+                    resourceType,
+                    name,
+                    cancellationToken: cancellationToken).ConfigureAwait(false)
+                : await kubernetes.CustomObjects.DeleteNamespacedCustomObjectWithHttpMessagesAsync(
+                    GroupVersion.Group,
+                    GroupVersion.Version,
+                    namespaceParameter,
+                    resourceType,
+                    name,
+                    cancellationToken: cancellationToken).ConfigureAwait(false);
+
+                return KubernetesJson.Deserialize<T>(response.Body.ToString());
+            },
+            cancellationToken);
+    }
+
+    public async IAsyncEnumerable<(WatchEventType, T)> WatchAsync<T>(
+        string? namespaceParameter = null,
+        [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        where T : CustomResource
+    {
+        var resourceType = GetResourceFor<T>();
+        var result = await ExecuteWithRetry(
+            DcpApiOperationType.Watch,
+            resourceType,
+            (kubernetes) =>
+            {
+                var responseTask = string.IsNullOrEmpty(namespaceParameter)
+                    ? kubernetes.CustomObjects.ListClusterCustomObjectWithHttpMessagesAsync(
+                        GroupVersion.Group,
+                        GroupVersion.Version,
+                        resourceType,
+                        watch: true,
+                        cancellationToken: cancellationToken)
+                    : kubernetes.CustomObjects.ListNamespacedCustomObjectWithHttpMessagesAsync(
+                        GroupVersion.Group,
+                        GroupVersion.Version,
+                        namespaceParameter,
+                        resourceType,
+                        watch: true,
+                        cancellationToken: cancellationToken);
+
+                return responseTask.WatchAsync<T, object>(null, cancellationToken);
+            },
+            cancellationToken).ConfigureAwait(false);
+
+        await foreach (var item in result)
+        {
+            yield return item;
+        }
+    }
+
+    public Task<Stream> GetLogStreamAsync<T>(
+        T obj,
+        string logStreamType,
+        bool? follow = true,
+        bool? timestamps = false,
+        CancellationToken cancellationToken = default) where T : CustomResource
+    {
+        var resourceType = GetResourceFor<T>();
+
+        ImmutableArray<(string name, string value)>? queryParams = [
+            (name: "follow", value: follow == true ? "true": "false"),
+            (name: "timestamps", value: timestamps == true ? "true" : "false"),
+            (name: "source", value: logStreamType)
+        ];
+
+        return ExecuteWithRetry(
+            DcpApiOperationType.GetLogSubresource,
+            resourceType,
+            async (kubernetes) =>
+            {
+                var response = await kubernetes.ReadSubResourceAsStreamAsync(
+                    GroupVersion.Group,
+                    GroupVersion.Version,
+                    resourceType,
+                    obj.Metadata.Name,
+                    Logs.SubResourceName,
+                    obj.Metadata.Namespace(),
+                    queryParams,
+                    cancellationToken
+                ).ConfigureAwait(false);
+
+                return response.Body;
+            },
+            cancellationToken
+        );
+    }
+
+    public void Dispose()
+    {
+        _kubeconfigReadSemaphore?.Dispose();
+        _kubernetes?.Dispose();
+    }
+
+    private static string GetResourceFor<T>() where T : CustomResource
+    {
+        if (!Model.Dcp.Schema.TryGet<T>(out var kindWithResource))
+        {
+            throw new InvalidOperationException($"Unknown custom resource type: {typeof(T).Name}");
+        }
+
+        return kindWithResource.Resource;
+    }
+
+    private Task<TResult> ExecuteWithRetry<TResult>(
+        DcpApiOperationType operationType,
+        string resourceType,
+        Func<DcpKubernetesClient, TResult> operation,
+        CancellationToken cancellationToken)
+    {
+        return ExecuteWithRetry<TResult>(
+            operationType,
+            resourceType,
+            (DcpKubernetesClient kubernetes) => Task.FromResult(operation(kubernetes)),
+            cancellationToken);
+    }
+
+    private async Task<TResult> ExecuteWithRetry<TResult>(
+        DcpApiOperationType operationType,
+        string resourceType,
+        Func<DcpKubernetesClient, Task<TResult>> operation,
+        CancellationToken cancellationToken)
+    {
+        var currentTimestamp = DateTime.UtcNow;
+        var delay = s_initialRetryDelay;
+        AspireEventSource.Instance.DcpApiCallStart(operationType, resourceType);
+
+        try
+        {
+            while (true)
+            {
+                try
+                {
+                    await EnsureKubernetesAsync(cancellationToken).ConfigureAwait(false);
+                    return await operation(_kubernetes!).ConfigureAwait(false);
+                }
+                catch (Exception e) when (IsRetryable(e))
+                {
+                    if (DateTime.UtcNow.Subtract(currentTimestamp) > MaxRetryDuration)
+                    {
+                        AspireEventSource.Instance.DcpApiCallTimeout(operationType, resourceType);
+                        throw;
+                    }
+
+                    await Task.Delay(delay, cancellationToken).ConfigureAwait(false);
+                    delay *= 2;
+                    AspireEventSource.Instance.DcpApiCallRetry(operationType, resourceType);
+                }
+            }
+        }
+        finally
+        {
+            AspireEventSource.Instance.DcpApiCallStop(operationType, resourceType);
+        }
+
+    }
+
+    private static bool IsRetryable(Exception ex) => ex is HttpRequestException || ex is KubeConfigException;
+
+    private readonly SemaphoreSlim _kubeconfigReadSemaphore = new(1);
+
+    private ResiliencePipeline? _resiliencePipeline;
+
+    private ResiliencePipeline GetReadKubeconfigResiliencePipeline()
+    {
+        if (_resiliencePipeline == null)
+        {
+            var configurationReadRetry = new RetryStrategyOptions()
+            {
+                ShouldHandle = new PredicateBuilder().Handle<KubeConfigException>(),
+                BackoffType = DelayBackoffType.Constant,
+                MaxRetryAttempts = dcpOptions.Value.KubernetesConfigReadRetryCount,
+                MaxDelay = TimeSpan.FromMilliseconds(dcpOptions.Value.KubernetesConfigReadRetryIntervalMilliseconds),
+                OnRetry = (retry) =>
+                {
+                    logger.LogDebug(
+                        "Waiting for Kubernetes configuration file at '{DcpKubeconfigPath}' (attempt {Iteration}).",
+                        locations.DcpKubeconfigPath,
+                        retry.AttemptNumber
+                        );
+                    return ValueTask.CompletedTask;
+                }
+            };
+
+            _resiliencePipeline = new ResiliencePipelineBuilder().AddRetry(configurationReadRetry).Build();
+        }
+
+        return _resiliencePipeline;
+    }
+
+    private async Task EnsureKubernetesAsync(CancellationToken cancellationToken = default)
+    {
+        // Return early before waiting for the semaphore if we can.
+        if (_kubernetes != null)
+        {
+            return;
+        }
+
+        await _kubeconfigReadSemaphore.WaitAsync(-1, cancellationToken).ConfigureAwait(false);
+
+        try
+        {
+            // Second chance shortcut if multiple threads got caught.
+            if (_kubernetes != null)
+            {
+                return;
+            }
+
+            // We retry reading the kubeconfig file because DCP takes a few moments to write
+            // it to disk. This retry pipeline will only be invoked by a single thread the
+            // rest will be held at the semaphore.
+            var readStopwatch = new Stopwatch();
+            readStopwatch.Start();
+
+            var pipeline = GetReadKubeconfigResiliencePipeline();
+            _kubernetes = await pipeline.ExecuteAsync<DcpKubernetesClient>(async (cancellationToken) =>
+            {
+                var fileInfo = new FileInfo(locations.DcpKubeconfigPath);
+                var config = await KubernetesClientConfiguration.BuildConfigFromConfigFileAsync(kubeconfig: fileInfo, useRelativePaths: false).ConfigureAwait(false);
+                readStopwatch.Stop();
+
+                logger.LogDebug(
+                    "Successfully read Kubernetes configuration from '{DcpKubeconfigPath}' after {DurationMs} milliseconds.",
+                    locations.DcpKubeconfigPath,
+                    readStopwatch.ElapsedMilliseconds
+                    );
+
+                return new DcpKubernetesClient(config);
+            }, cancellationToken).ConfigureAwait(false);
+        }
+        finally
+        {
+            _kubeconfigReadSemaphore.Release();
+        }
+    }
+}
---
+++ src/Aspire.Hosting/DistributedApplicationBuilder.cs
@@ -0,0 +1,274 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Dashboard;
+using Aspire.Hosting.Dcp;
+using Aspire.Hosting.Lifecycle;
+using Aspire.Hosting.Publishing;
+using Aspire.Hosting.Utils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Aspire.Hosting;
+
+/// <summary>
+/// A builder for creating instances of <see cref="DistributedApplication"/>.
+/// </summary>
+public class DistributedApplicationBuilder : IDistributedApplicationBuilder
+{
+    private const string HostingDiagnosticListenerName = "Aspire.Hosting";
+    private const string ApplicationBuildingEventName = "DistributedApplicationBuilding";
+    private const string ApplicationBuiltEventName = "DistributedApplicationBuilt";
+    private const string BuilderConstructingEventName = "DistributedApplicationBuilderConstructing";
+    private const string BuilderConstructedEventName = "DistributedApplicationBuilderConstructed";
+
+    private const string DisableOtlpApiKeyAuthKey = "DOTNET_DISABLE_OTLP_API_KEY_AUTH";
+
+    private readonly HostApplicationBuilder _innerBuilder;
+
+    /// <inheritdoc />
+    public IHostEnvironment Environment => _innerBuilder.Environment;
+
+    /// <inheritdoc />
+    public ConfigurationManager Configuration => _innerBuilder.Configuration;
+
+    /// <inheritdoc />
+    public IServiceCollection Services => _innerBuilder.Services;
+
+    /// <inheritdoc />
+    public string AppHostDirectory { get; }
+
+    /// <inheritdoc />
+    public DistributedApplicationExecutionContext ExecutionContext { get; }
+
+    /// <inheritdoc />
+    public IResourceCollection Resources { get; } = new ResourceCollection();
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DistributedApplicationBuilder"/> class with the specified options.
+    /// </summary>
+    /// <param name="args">The arguments provided to the builder.</param>
+    public DistributedApplicationBuilder(string[] args) : this(new DistributedApplicationOptions { Args = args })
+    {
+        ArgumentNullException.ThrowIfNull(args);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="DistributedApplicationBuilder"/> class with the specified options.
+    /// </summary>
+    /// <param name="options">The options for the distributed application.</param>
+    public DistributedApplicationBuilder(DistributedApplicationOptions options)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+
+        var innerBuilderOptions = new HostApplicationBuilderSettings();
+        LogBuilderConstructing(options, innerBuilderOptions);
+        _innerBuilder = new HostApplicationBuilder(innerBuilderOptions);
+
+        _innerBuilder.Logging.AddFilter("Microsoft.Hosting.Lifetime", LogLevel.Warning);
+        _innerBuilder.Logging.AddFilter("Microsoft.AspNetCore.Server.Kestrel", LogLevel.None);
+
+        AppHostDirectory = options.ProjectDirectory ?? _innerBuilder.Environment.ContentRootPath;
+
+        _innerBuilder.Configuration.AddInMemoryCollection(new Dictionary<string, string?>
+        {
+            // Make the app host directory available to the application via configuration
+            ["AppHost:Directory"] = AppHostDirectory
+        });
+
+        // Core things
+        _innerBuilder.Services.AddSingleton(sp => new DistributedApplicationModel(Resources));
+        _innerBuilder.Services.AddHostedService<DistributedApplicationLifecycle>();
+        _innerBuilder.Services.AddHostedService<DistributedApplicationRunner>();
+        _innerBuilder.Services.AddSingleton(options);
+        _innerBuilder.Services.AddSingleton<ResourceNotificationService>();
+        _innerBuilder.Services.AddSingleton<ResourceLoggerService>();
+
+        // Dashboard
+        _innerBuilder.Services.AddSingleton<DashboardServiceHost>();
+        _innerBuilder.Services.AddHostedService<DashboardServiceHost>(sp => sp.GetRequiredService<DashboardServiceHost>());
+        _innerBuilder.Services.AddLifecycleHook<DashboardManifestExclusionHook>();
+
+        // DCP stuff
+        _innerBuilder.Services.AddSingleton<ApplicationExecutor>();
+        _innerBuilder.Services.AddSingleton<IDashboardEndpointProvider, HostDashboardEndpointProvider>();
+        _innerBuilder.Services.AddSingleton<IDcpDependencyCheckService, DcpDependencyCheck>();
+        _innerBuilder.Services.AddHostedService<DcpHostService>();
+
+        // We need a unique path per application instance
+        _innerBuilder.Services.AddSingleton(new Locations());
+        _innerBuilder.Services.AddSingleton<IKubernetesService, KubernetesService>();
+
+        // Publishing support
+        ConfigurePublishingOptions(options);
+        _innerBuilder.Services.AddLifecycleHook<Http2TransportMutationHook>();
+        _innerBuilder.Services.AddKeyedSingleton<IDistributedApplicationPublisher, ManifestPublisher>("manifest");
+        _innerBuilder.Services.AddKeyedSingleton<IDistributedApplicationPublisher, DcpPublisher>("dcp");
+
+        ExecutionContext = _innerBuilder.Configuration["Publishing:Publisher"] switch
+        {
+            "manifest" => new DistributedApplicationExecutionContext(DistributedApplicationOperation.Publish),
+            _ => new DistributedApplicationExecutionContext(DistributedApplicationOperation.Run)
+        };
+
+        _innerBuilder.Services.AddSingleton<DistributedApplicationExecutionContext>(ExecutionContext);
+        LogBuilderConstructed(this);
+    }
+
+    private static bool IsOtlpApiKeyAuthDisabled(IConfiguration configuration)
+    {
+        return configuration.GetBool(DisableOtlpApiKeyAuthKey) ?? false;
+    }
+
+    private void ConfigurePublishingOptions(DistributedApplicationOptions options)
+    {
+        var switchMappings = new Dictionary<string, string>()
+        {
+            { "--publisher", "Publishing:Publisher" },
+            { "--output-path", "Publishing:OutputPath" },
+            { "--dcp-cli-path", "DcpPublisher:CliPath" },
+            { "--container-runtime", "DcpPublisher:ContainerRuntime" },
+            { "--dependency-check-timeout", "DcpPublisher:DependencyCheckTimeout" },
+        };
+        _innerBuilder.Configuration.AddCommandLine(options.Args ?? [], switchMappings);
+        _innerBuilder.Services.Configure<PublishingOptions>(_innerBuilder.Configuration.GetSection(PublishingOptions.Publishing));
+        _innerBuilder.Services.Configure<DcpOptions>(
+            o => o.ApplyApplicationConfiguration(
+                options,
+                dcpPublisherConfiguration: _innerBuilder.Configuration.GetSection(DcpOptions.DcpPublisher),
+                publishingConfiguration: _innerBuilder.Configuration.GetSection(PublishingOptions.Publishing),
+                coreConfiguration: _innerBuilder.Configuration
+            )
+        );
+    }
+
+    /// <inheritdoc />
+    public DistributedApplication Build()
+    {
+        AspireEventSource.Instance.DistributedApplicationBuildStart();
+        try
+        {
+            LogAppBuilding(this);
+
+            // AddResource(resource) validates that a name is unique but it's possible to add resources directly to the resource collection.
+            // Validate names for duplicates while building the application.
+            foreach (var duplicateResourceName in Resources.GroupBy(r => r.Name, StringComparers.ResourceName)
+                .Where(g => g.Count() > 1)
+                .Select(g => g.Key))
+            {
+                throw new DistributedApplicationException($"Multiple resources with the name '{duplicateResourceName}'. Resource names are case-insensitive.");
+            }
+
+            if (!IsOtlpApiKeyAuthDisabled(_innerBuilder.Configuration))
+            {
+                // Set a random API key for the OTLP exporter.
+                // Passed to apps as a standard OTEL attribute to include in OTLP requests and the dashboard to validate.
+                _innerBuilder.Configuration.AddInMemoryCollection(
+                    new Dictionary<string, string?>
+                    {
+                        ["AppHost:OtlpApiKey"] = TokenGenerator.GenerateToken()
+                    }
+                );
+            }
+
+            var application = new DistributedApplication(_innerBuilder.Build());
+            LogAppBuilt(application);
+            return application;
+        }
+        finally
+        {
+            AspireEventSource.Instance.DistributedApplicationBuildStop();
+        }
+    }
+
+    /// <inheritdoc />
+    public IResourceBuilder<T> AddResource<T>(T resource) where T : IResource
+    {
+        ArgumentNullException.ThrowIfNull(resource);
+
+        if (Resources.FirstOrDefault(r => string.Equals(r.Name, resource.Name, StringComparisons.ResourceName)) is { } existingResource)
+        {
+            throw new DistributedApplicationException($"Cannot add resource of type '{resource.GetType()}' with name '{resource.Name}' because resource of type '{existingResource.GetType()}' with that name already exists. Resource names are case-insensitive.");
+        }
+
+        Resources.Add(resource);
+        return CreateResourceBuilder(resource);
+    }
+
+    /// <inheritdoc />
+    public IResourceBuilder<T> CreateResourceBuilder<T>(T resource) where T : IResource
+    {
+        ArgumentNullException.ThrowIfNull(resource);
+
+        var builder = new DistributedApplicationResourceBuilder<T>(this, resource);
+        return builder;
+    }
+
+    private static DiagnosticListener LogBuilderConstructing(DistributedApplicationOptions appBuilderOptions, HostApplicationBuilderSettings hostBuilderOptions)
+    {
+        var diagnosticListener = new DiagnosticListener(HostingDiagnosticListenerName);
+
+        if (diagnosticListener.IsEnabled() && diagnosticListener.IsEnabled(BuilderConstructingEventName))
+        {
+            Write(diagnosticListener, BuilderConstructingEventName, (appBuilderOptions, hostBuilderOptions));
+        }
+
+        return diagnosticListener;
+    }
+
+    private static DiagnosticListener LogBuilderConstructed(DistributedApplicationBuilder builder)
+    {
+        var diagnosticListener = new DiagnosticListener(HostingDiagnosticListenerName);
+
+        if (diagnosticListener.IsEnabled() && diagnosticListener.IsEnabled(BuilderConstructedEventName))
+        {
+            Write(diagnosticListener, BuilderConstructedEventName, builder);
+        }
+
+        return diagnosticListener;
+    }
+
+    private static DiagnosticListener LogAppBuilding(DistributedApplicationBuilder appBuilder)
+    {
+        var diagnosticListener = new DiagnosticListener(HostingDiagnosticListenerName);
+
+        if (diagnosticListener.IsEnabled() && diagnosticListener.IsEnabled(ApplicationBuildingEventName))
+        {
+            Write(diagnosticListener, ApplicationBuildingEventName, appBuilder);
+        }
+
+        return diagnosticListener;
+    }
+
+    private static DiagnosticListener LogAppBuilt(DistributedApplication app)
+    {
+        var diagnosticListener = new DiagnosticListener(HostingDiagnosticListenerName);
+
+        if (diagnosticListener.IsEnabled() && diagnosticListener.IsEnabled(ApplicationBuiltEventName))
+        {
+            Write(diagnosticListener, ApplicationBuiltEventName, app);
+        }
+
+        return diagnosticListener;
+    }
+
+    // Remove when https://github.com/dotnet/runtime/pull/78532 is merged and consumed by the used SDK.
+#if NET7_0
+        [UnconditionalSuppressMessage("AOT", "IL3050:RequiresDynamicCode",
+            Justification = "DiagnosticSource is used here to pass objects in-memory to code using HostFactoryResolver. This won't require creating new generic types.")]
+#endif
+    [UnconditionalSuppressMessage("ReflectionAnalysis", "IL2026:UnrecognizedReflectionPattern",
+        Justification = "The values being passed into Write are being consumed by the application already.")]
+    private static void Write<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicProperties)] T>(
+        DiagnosticListener diagnosticSource,
+        string name,
+        T value)
+    {
+        diagnosticSource.Write(name, value);
+    }
+}
