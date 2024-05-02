3266243b663f2cc4eaeb5b205af9e7ca7089456c
nguyenchihai349outlook <166897865+nguyenchihai349outlook@users.noreply.github.com>
2024-05-02 22:08:09 +0700

    Initialize smit/kubernetesList

---
+++ src/Aspire.Dashboard/wwwroot/Aspire.Dashboard.lib.module.js
@@ -0,0 +1,9 @@
+import * as LibraryExports from "/_content/Microsoft.FluentUI.AspNetCore.Components/Microsoft.FluentUI.AspNetCore.Components.lib.module.js";
+
+export function beforeServerStart(options, extensions) {
+    LibraryExports.beforeStart(options, extensions);
+}
+
+export function afterServerStarted(blazor) {
+    LibraryExports.afterStarted(blazor);
+}
